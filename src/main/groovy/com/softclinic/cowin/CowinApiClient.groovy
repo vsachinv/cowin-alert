240eeb304c26cc850b45451f881e1f4b25a38710
sachinverma <v.sachin.v@gmail.com>
2021-05-18 02:05:23 +0530

    Added additional header for heroku server to work

--- src/main/groovy/com/softclinic/cowin/CowinApiClient.groovy
+++ src/main/groovy/com/softclinic/cowin/CowinApiClient.groovy
@@ -10,10 +10,12 @@ import io.reactivex.Flowable
 
 import static io.micronaut.http.HttpHeaders.ACCEPT
 import static io.micronaut.http.HttpHeaders.USER_AGENT
+import static io.micronaut.http.HttpHeaders.HOST
 
 @Client(CowinApiConfiguration.COWIN_API_URL)
 @Header(name = USER_AGENT, value = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
 @Header(name = ACCEPT, value = "application/json")
+@Header(name = HOST, value = "cdn-api.co-vin.in")
 interface CowinApiClient {
     @Get('/admin/location/states')
     StateApiDTO fetchStates()
