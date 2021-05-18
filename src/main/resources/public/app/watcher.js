e34fed8edc48a2882789d6d0d6e79882d7b15894
sachinverma <v.sachin.v@gmail.com>
2021-05-19 01:20:01 +0530

    Added check for Dose1 and Dose2 as well.

--- src/main/resources/public/app/watcher.js
+++ src/main/resources/public/app/watcher.js
@@ -50,16 +50,17 @@ $(function () {
         var districtId = $('#districtId').val();
         var vaccineType = $('input[name=vaccineType]:checked').val();
         var ageLimit = $('input[name=ageLimit]:checked').val();
+        var doseType = $('input[name=doseType]:checked').val();
         var refreshInterval = $('input[name=refreshInterval]').val();
-        if (!districtId || !vaccineType || !ageLimit || !refreshInterval) {
+        if (!districtId || !vaccineType || !ageLimit || !refreshInterval || !doseType) {
             alert('Please enter input');
             return
         }
         $(this).prop('disabled', true);
         $('#result').removeClass('invisible');
-        cowinCoriApiSearch(districtId, vaccineType, ageLimit);
+        cowinCoriApiSearch(districtId, vaccineType, ageLimit, doseType);
         timerId = setInterval(function () {
-            cowinCoriApiSearch(districtId, vaccineType, ageLimit);
+            cowinCoriApiSearch(districtId, vaccineType, ageLimit, doseType);
         }, 1000 * 60 * refreshInterval);
 
     });
@@ -78,14 +79,14 @@ $(function () {
         return [day, month, year].join('-');
     };
 
-    var cowinCoriApiSearch = function (districtId, vaccineType, ageLimit) {
+    var cowinCoriApiSearch = function (districtId, vaccineType, ageLimit, doseType) {
         var apiUrl = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=:districtId:&date=:DATE:';
         var centersNames = '';
         $.getJSON(apiUrl.replace(':districtId:', districtId).replace(':DATE:', currentDate()), function (data) {
             var newFoundCenters = '';
             $.each(data.centers, function (index, element) {
                 var sessions = $.grep(element.sessions, function (it, j) {
-                    if ((vaccineType == 'ANY' || it.vaccine == vaccineType) && it.available_capacity > 2 && it.min_age_limit == ageLimit) {
+                    if ((vaccineType == 'ANY' || it.vaccine == vaccineType) && ((doseType == 'FIRST' && it.available_capacity_dose1 > 2) || (doseType == 'SECOND' && it.available_capacity_dose2 > 2)) && it.min_age_limit == ageLimit) {
                         return true
                     }
                     return false
